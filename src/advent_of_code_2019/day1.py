bfcc9a7ca8173b3555c94b16d1953e34bd4b3d70
Keir Davis <keirjdavis@gmail.com>
2020-12-01 20:42:11 +0000

    feat(2019day1): add fuel's fuel calculation

--- src/advent_of_code_2019/day1.py
+++ src/advent_of_code_2019/day1.py
@@ -17,6 +17,27 @@ def sum_of_fuel_requirements():
 
     return fuel_requirement
 
+def calculate_fuels_fuel_requirements():
+    total_fuel_requirement = 0
+
+    for line in open(os.path.dirname(__file__) + os.path.sep + "inputs/day1input.txt", "r").readlines():
+        # Convert each line to an integer as it's stored as a string in a text file
+        mass = int(line)
+
+        current_module_fuel_requirement = 0
+        last_fuel_requirement = mass
+
+        # Calculate the fuel requirement for the fuel needed for the mass
+        while True:
+            last_fuel_requirement = _calculate_fuel_requirement(last_fuel_requirement)
+            if last_fuel_requirement <= 0:
+                break
+            current_module_fuel_requirement += last_fuel_requirement
+        
+        total_fuel_requirement += current_module_fuel_requirement
+
+    return total_fuel_requirement
+
 def _calculate_fuel_requirement(mass):
     """
     Helper function to calculate mass
@@ -26,3 +47,6 @@ def _calculate_fuel_requirement(mass):
 if __name__ == "__main__":
     fuel_requirement = sum_of_fuel_requirements()
     print(f"The fuel required to launch is: {fuel_requirement}")
+
+    mass_fuel_and_fuel_requirements = calculate_fuels_fuel_requirements()
+    print(f"The fuel required to launch with extra fuel is: {mass_fuel_and_fuel_requirements}")

41c877454278b03b2e562fa46e76264c6974547e
Keir Davis <keirjdavis@gmail.com>
2020-12-01 13:03:20 +0000

    feat(day1): add first day1 test

---
+++ src/AdventOfCode.Tests/Day1Tests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests
+{
+	[TestClass]
+	public class Day1Tests
+	{
+		[TestMethod]
+		public async Task ProductOfTwoEntriesThatSumTo2020()
+		{
+			var inputs = await File.ReadAllLinesAsync("Inputs/day1input.txt");
+
+			var expenses = inputs.Select(input => int.Parse(input)).ToList();
+
+			var expense = expenses.SelectMany(
+				expense1 => expenses.Select(
+						expense2 => new
+						{
+							Expense1 = expense1,
+							Expense2 = expense2,
+							Sum = expense1 + expense2,
+							Product = expense1 * expense2
+						})
+				)
+				.Where(a => a.Sum == 2020)
+				.FirstOrDefault();
+
+			System.Console.WriteLine($"Product of two entries that sum to 2020: {expense.Product}");
+		}
+	}
+}

afd9308293ec3b1dcae0c9392a1bde94d2b35e35
Keir Davis <keirjdavis@gmail.com>
2020-12-02 09:37:22 +0000

    feat(2020day2): add toboggan valid password count test

--- src/AdventOfCode.2020.Tests/Day2Tests.cs
+++ src/AdventOfCode.2020.Tests/Day2Tests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,13 +10,12 @@ namespace AdventOfCode.Tests
 	public class Day2Tests
 	{
 		[TestMethod]
-		public async Task ValidPasswordsCount()
+		public async Task SledRentalValidPasswordsCount()
 		{
 			var inputs = await File.ReadAllLinesAsync("Inputs/day2input.txt");
 
 			var validPasswords = 0;
 
-			// 8-16 v: vfvvhvvtvvvjdvvfvv
 			foreach (var input in inputs)
 			{
 				var rangeEndIndex = input.IndexOf(" ");
@@ -28,7 +28,7 @@ namespace AdventOfCode.Tests
 				var minCountRequirement = int.Parse(ranges[0]);
 				var maxCountRequirement = int.Parse(ranges[1]);
 
-				var password = input.Substring(requirementsEndIndex + 1);
+				var password = input.Substring(requirementsEndIndex + 2);
 				var letterExistCount = password.Where(letter => letter.ToString() == requiredLetter).Count();
 
 				if ((minCountRequirement <= letterExistCount) && (letterExistCount <= maxCountRequirement))
@@ -37,7 +37,39 @@ namespace AdventOfCode.Tests
 				}
 			}
 
-			System.Console.WriteLine($"There are {validPasswords} passwords valid.");
+			System.Console.WriteLine($"There are {validPasswords} passwords valid at the sled rental shop down the street.");
+		}
+
+		[TestMethod]
+		public async Task TobogganValidPasswordsCount()
+		{
+			var inputs = await File.ReadAllLinesAsync("Inputs/day2input.txt");
+
+			var validPasswords = 0;
+
+			foreach (var input in inputs)
+			{
+				var rangeEndIndex = input.IndexOf(" ");
+				var requirementsEndIndex = input.IndexOf(": ");
+
+				var indexRange = input.Substring(0, rangeEndIndex);
+				var requiredLetter = char.Parse(input.Substring(rangeEndIndex + 1, requirementsEndIndex - (rangeEndIndex + 1)));
+
+				var indexes = indexRange.Split("-");
+				var firstIndexRequirement = int.Parse(indexes[0]);
+				var secondIndexRequirement = int.Parse(indexes[1]);
+
+				var password = input.Substring(requirementsEndIndex + 2);
+
+				var passwordFirstCharAt = password[firstIndexRequirement - 1];
+				var passwordSecondCharAt = password[secondIndexRequirement - 1];
+
+				if (passwordFirstCharAt == requiredLetter ^ passwordSecondCharAt == requiredLetter)
+				{
+					validPasswords++;
+				}
+			}
+			System.Console.WriteLine($"There are {validPasswords} passwords valid for the Official Toboggan Corporate Authentication System.");
 		}
 	}
 }

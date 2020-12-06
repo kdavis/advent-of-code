209550257c2e79a33e1ae95bc369a96b4860699a
Keir Davis <keirjdavis@gmail.com>
2020-12-06 13:28:40 +0000

    feat(2020day6): add 2nd part of day 6

--- src/AdventOfCode.2020.Tests/Day6Tests.cs
+++ src/AdventOfCode.2020.Tests/Day6Tests.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode.Tests
 	[TestClass]
 	public class Day6Tests
 	{
-		public List<string> GroupAnswerIdentifier(string input)
+		public List<string> UniqueAnswerIdentifier(string input)
 		{
 			var groupOfGroups = input.Split("\r\n\r\n")
 				.Select(group => string.Join("", group.Replace("\r\n", "").ToList().Distinct())).ToList();
@@ -18,10 +18,33 @@ namespace AdventOfCode.Tests
 			return groupOfGroups;
 		}
 
-		[TestMethod]
-		public void GroupAnswerIdentifierTest()
+		public List<List<char>> GroupAnswerIdentifier(string input)
 		{
-			var groupOfGroups = GroupAnswerIdentifier(@"abc
+			var groups = input.Split("\r\n\r\n");
+
+			var distinctGroupAnswers = new List<List<char>>();
+
+			foreach (var group in groups)
+            {
+				var answers = group.Split("\r\n").Where(q => !string.IsNullOrEmpty(q));
+
+				var allCharacters = answers.SelectMany(q => q.ToList()).Distinct();
+
+				var charactersFoundInAnswers = allCharacters.Where(character =>
+				{
+					return answers.All(answer => answer.Contains(character));
+				}).ToList();
+
+				distinctGroupAnswers.Add(charactersFoundInAnswers);
+			}
+
+			return distinctGroupAnswers;
+		}
+
+		[TestMethod]
+		public void UniqueAnswerIdentifierTest()
+		{
+			var groupOfGroups = UniqueAnswerIdentifier(@"abc
 
 a
 b
@@ -46,14 +69,50 @@ b");
 		}
 
 		[TestMethod]
+		public async Task UniqueAnswerIdentifierTask()
+		{
+			var input = await File.ReadAllTextAsync("Inputs/day6input.txt");
+
+			var groupOfGroups = UniqueAnswerIdentifier(input);
+
+			Console.WriteLine($"Sum of distinct group answers: {groupOfGroups.Sum(group => group.Length)}");
+		}
+
+		[TestMethod]
+		public void GroupAnswerIdentifierTest()
+		{
+			var groupOfGroups = GroupAnswerIdentifier(@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b");
+
+			Assert.AreEqual(3, groupOfGroups[0].Count);
+			Assert.AreEqual(0, groupOfGroups[1].Count);
+			Assert.AreEqual(1, groupOfGroups[2].Count);
+			Assert.AreEqual(1, groupOfGroups[3].Count);
+			Assert.AreEqual(1, groupOfGroups[4].Count);
+			Assert.AreEqual(6, groupOfGroups.Sum(group => group.Count));
+		}
+
+		[TestMethod]
 		public async Task GroupAnswerIdentifierTask()
 		{
 			var input = await File.ReadAllTextAsync("Inputs/day6input.txt");
 
 			var groupOfGroups = GroupAnswerIdentifier(input);
 
-			Console.WriteLine($"Sum of distinct group answers: {groupOfGroups.Sum(group => group.Length)}");
-
+			Console.WriteLine($"Sum of all answered distinct group answers: {groupOfGroups.Sum(group => group.Count)}");
 		}
 	}
 }
